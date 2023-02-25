a3682dc9363f7b47e8d48f0aed2224b35c45b122
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-02-25 14:39:51 +0000

    Added Login Component;

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'lists', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
   { path: 'new-list', component: NewListComponent },
   { path: 'lists', component: TaskViewComponent },
   { path: 'lists/:listId', component: TaskViewComponent },
