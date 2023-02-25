12935febc009f4517c880ed412b269a5e7262bc4
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-02-25 09:28:34 +0000

    Added calls to retrieve the lists and tasks for a specific list. Also the task-view comp template now dynamically displays them. Added routerLinkActive to set the class to is-active for the selected list;

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-task-view',
   templateUrl: './task-view.component.html',
   styleUrls: ['./task-view.component.scss']
 })
-export class TaskViewComponent {
+export class TaskViewComponent implements OnInit {
+
+  public lists: any[] = [];
+  public tasks: any[] = [];
+
+  constructor (
+    private taskService: TaskService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      if (params['listId']) {
+        this.taskService.getTasks(params['listId']).subscribe((tasks: any) => {
+          this.tasks = tasks;
+        })
+      };
+    });
+
+    this.taskService.getLists().subscribe((lists: any) => {
+      this.lists = lists;
+    })
+  }
+
 
 }
