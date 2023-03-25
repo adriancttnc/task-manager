29c7ff5dbcfee651f22e869e4c4574b8477409ac
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-03-25 16:12:55 +0000

    Removed ToDo from a comment;

--- api/app.js
+++ api/app.js
@@ -437,7 +437,7 @@ app.listen(3000, () => {
 ************************************************************/
 // ToDo - Ensure you can handle illegal Ids.
 // ToDo - Prevent failed calls to the backend. (You get a failed call whenever the access token expires and needs to be renewed)
-// ToDo - Change all new/edit pages to pop-up modals.
+// Change all new/edit pages to pop-up modals.
 // ToDo - Implement a form component and use it throughout the app.
 // ToDo - Add unitTesting.
 // ToDo - Add separate webRequest functions for lists and tasks.
