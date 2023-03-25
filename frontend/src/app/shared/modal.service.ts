278deeee0c61767cdda708e9aeb0fe80459e3680
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-03-25 14:24:44 +0000

    Removed dialogRef from modalService;

--- frontend/src/app/shared/modal.service.ts
+++ frontend/src/app/shared/modal.service.ts
@@ -7,7 +7,6 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 })
 export class ModalService {
 
-  public dialogRef: any;
   private dialogConfig: MatDialogConfig = {
     disableClose: true,
     panelClass: 'title'
@@ -18,21 +17,10 @@ export class ModalService {
   ) {}
 
   public openModal<T = unknown> (component: ComponentType<T>, config?: MatDialogConfig) {
-     this.dialogRef = this.dialog.open(component, {
+    return this.dialog.open(component, {
       data: config?.data,
       ...this.dialogConfig
     });
-
-    this.dialogRef.afterClosed().subscribe((response: unknown) => {
-      return response;
-    })
-
-    return this.dialogRef;
   }
 
-  public closeModal () {
-    if (this.dialogRef?.close) {
-      this.dialogRef.close();
-    }
-  }
 }
