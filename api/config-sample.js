b450b554ce02ecbd4ba5456e2db8af13a2452fff
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-05-01 21:08:16 +0100

    Created config-sample.js and config.js(untracked).

--- api/config-sample.js
+++ api/config-sample.js
@@ -5,11 +5,19 @@
 
 module.exports = {
   smtp: {                             // Authentication details for the email server.
-    username: '',
-    password: '',
-    server:   '',
-    port:     '',
-    ssl:      ''
+    username:    '',
+    displayName: '',
+    password:    '',
+    server:      '',
+    service:     '',
+    port:        '',
+    ssl:         ''
+  },
+  oAuth2: {
+    enabled:      '',
+    clientId:     '',
+    accessToken:  '',
+    refreshToken: ''
   },
   email: {                            // Settings that apply when sending emails.
     receiverOverwrite: ''             // Sends all emails to this email if populated. (for testing)
@@ -20,5 +28,12 @@ module.exports = {
     port:     '27017',
     username: '',
     password: ''
+  },
+  registration: {                           // Settings for the user registration.
+    refreshTokenLifespan:       10,         // Days for how long the refreshToken lasts.
+    accessTokenLifespan:        15,         // Minutes for how long the access token lasts.
+    forgotPasswordKeyLifespan:  '15s',      // Time for how long the forgot password key lasts. String along with the unit for others ('60s', '15m', '1h', '2d')
+    bcryptCostFactor:           12,         // Value that specifies the power of 2 for the number of iterations when hashing the password.
+    minPasswordLength: 8
   }
 }
