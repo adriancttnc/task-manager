e1bf99822fed61a74f559fb82edf551d07184fcb
ADRIAN-DESKTOP\adria <adriancttnc@yahoo.com>
2023-05-01 21:07:13 +0100

    Created userPasswordEvent model.

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,9 +1,11 @@
 const { List } = require('./list.model');
 const { Task } = require('./task.model');
 const { User } = require('./user.model');
+const { UserPasswordEvent } = require('./userPasswordEvent')
 
 module.exports = {
   List,
   Task,
-  User
+  User,
+  UserPasswordEvent
 };
